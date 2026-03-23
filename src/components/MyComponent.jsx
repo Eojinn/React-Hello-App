f84d73b8aa86f2eecd275f9365d74daff7e02182
Eojinn <aj41212@gmail.com>
2026-03-23 14:19:34 +0900

    useState add

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -3,31 +3,42 @@ import PropTypes from 'prop-types';
 
 //Class 형태의 컴포넌트
 class MyComponent extends Component {
+
+    //Event Handler함수를 Arrow함수가 아니라 기존함수로 선언했을 경우 bind(this)가 필요함
+    // constructor(props) {
+    //     super(props);
+    //     this.handleDecrement = this.handleDecrement.bind(this);
+    // }
+
     //컴포넌트 내부에 상태변수를 포함하는 state 객체선언
     state = {
         value: 0,
     };
-    //Event Handler 함수 선언
+    //Event Handler 함수선언
+    //handleDecrement() {    
     handleDecrement = () => {
         this.setState({
             value: this.state.value - 1
         });
-    };
+    }; //handleDecrement  
+
     //메서드 오버라이딩
     render() {
         //Destructuring Assignment 적용해서 변수 선언
         const {name, age} = this.props;
         const {value} = this.state;
-        const{handleDecrement} = this;
+        const {handleDecrement} = this;
+
         return (
             <div>
                 <h2>클래스 형태의 컴포넌트</h2>
                 <h3>Hello!! {name} / {age}</h3>
                 <p>상태변수 value = {value}</p>
-                <button onClick={()/*파라미터*/ =>(
-                    this.setState({/*바디*/
-                        value: this.state.value + 1
-                    }))}>증가</button>
+                <button onClick={() => (
+                    this.setState({
+                        value: value + 1
+                    })
+                )}>증가</button>
                 <button onClick={handleDecrement}>감소</button>
             </div>
         );
