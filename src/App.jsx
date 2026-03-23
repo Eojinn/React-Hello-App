d125aeb79c4428c3e225cd09142c358ae3feecf8
Eojinn <aj41212@gmail.com>
2026-03-23 13:05:29 +0900

    hello react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MyComponent from './components/MyComponent';
+import MyComponentFunc from './components/MyComponentFunc';
+
+//function app(){}
+const App = () => {
+  return (
+    <>
+      <MyComponent name="리액트" />
+      <MyComponent name={3}/>
+      <hr />
+      <MyComponentFunc />
+
+    </>
+  );
+};
+
+export default App;
